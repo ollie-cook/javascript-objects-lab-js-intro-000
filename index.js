679c1b2ea9be39079a43aa50bc37c73f302a9d23
Oliver Cook <olliewatford@gmail.com>
2020-06-24 13:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ var updateObjectWithKeyAndValue = (object,key,value) =>{
 var destructivelyUpdateObjectWithKeyAndValue = (object,key,value) => {
   return Object.assign(object,{[key]:value})
 }
+
+var deleteFromObjectByKey = (object,key) => { 
+ var object2 = object; 
+ delete object2[key];
+}
